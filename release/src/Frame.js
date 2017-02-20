af81f4027509e50c527f0930a6499738ee00a075
Mr.doob <info@mrdoob.com>
2017-02-19 18:15:39 -0800

    Release: Now using WebAudio API.

--- release/src/Frame.js
+++ release/src/Frame.js
@@ -1,3 +1,7 @@
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
 var FRAME = ( function () {
 
 	var dom = null;
---
+++ release/src/WebAudio.js
@@ -0,0 +1,70 @@
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+function WebAudio() {
+
+	var context = new AudioContext();
+	var source, buffer;
+
+	var startAt = 0;
+	var paused = true;
+
+	function load( url ) {
+
+		var request = new XMLHttpRequest();
+		request.open( 'GET', url, true );
+		request.responseType = 'arraybuffer';
+		request.addEventListener( 'load', function ( event ) {
+			context.decodeAudioData( event.target.response, function ( data ) {
+				buffer = data;
+			} );
+		} );
+		request.send();
+
+	}
+
+	function play() {
+
+		if ( paused === false ) return;
+
+		source = context.createBufferSource();
+		source.buffer = buffer;
+		source.start();
+		source.connect( context.destination );
+
+		startAt = context.currentTime;
+		paused = false;
+
+	}
+
+	function pause() {
+
+		if ( paused === true ) return;
+
+		source.stop();
+		source.disconnect( context.destination );
+
+		paused = true;
+
+	}
+
+	return {
+		play: play,
+		pause: pause,
+		get currentTime() {
+			if ( paused === true ) return 0;
+			return context.currentTime - startAt;
+		},
+		set currentTime( value ) {
+			context.currenTime = value;
+		},
+		set src( url ) {
+			load( url );
+		},
+		get paused() {
+			return paused;
+		}
+	}
+
+}
