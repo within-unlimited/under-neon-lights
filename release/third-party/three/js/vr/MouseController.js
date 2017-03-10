13459aa1599d2ce63d847f1c543f5082b2e2651b
jonobr1 <correspondence@jonobr1.com>
2017-03-10 01:07:04 -0800

    fixed touch controls

--- release/third-party/three/js/vr/MouseController.js
+++ release/third-party/three/js/vr/MouseController.js
@@ -25,8 +25,8 @@ THREE.MouseController = function ( domElement ) {
 		e.preventDefault();
 		scope.visible = true;
 		mouse.set( e.clientX, e.clientY );
-		dragging = true;
 		scope.dispatchEvent( { type: 'mousedown' } );
+		dragging = true;
 
 	};
 
@@ -60,8 +60,9 @@ THREE.MouseController = function ( domElement ) {
 
 		scope.visible = true;
 		mouse.set( touch.pageX, touch.pageY );
-		dragging = true;
+		scope.update();
 		scope.dispatchEvent( { type: 'mousedown' } );
+		dragging = true;
 
 	};
 
@@ -137,36 +138,36 @@ THREE.MouseController = function ( domElement ) {
 	};
 
 	function debounce(func, wait, immediate) {
-    var timeout, args, context, timestamp, result;
+		var timeout, args, context, timestamp, result;
 
-    var later = function() {
-      var last = Date.now() - timestamp;
+		var later = function() {
+			var last = Date.now() - timestamp;
 
-      if (last < wait && last >= 0) {
-        timeout = setTimeout(later, wait - last);
-      } else {
-        timeout = null;
-        if (!immediate) {
-          result = func.apply(context, args);
-          if (!timeout) context = args = null;
-        }
-      }
-    };
+			if (last < wait && last >= 0) {
+				timeout = setTimeout(later, wait - last);
+			} else {
+				timeout = null;
+				if (!immediate) {
+					result = func.apply(context, args);
+					if (!timeout) context = args = null;
+				}
+			}
+		};
 
-    return function() {
-      context = this;
-      args = arguments;
-      timestamp = Date.now();
-      var callNow = immediate && !timeout;
-      if (!timeout) timeout = setTimeout(later, wait);
-      if (callNow) {
-        result = func.apply(context, args);
-        context = args = null;
-      }
+		return function() {
+			context = this;
+			args = arguments;
+			timestamp = Date.now();
+			var callNow = immediate && !timeout;
+			if (!timeout) timeout = setTimeout(later, wait);
+			if (callNow) {
+				result = func.apply(context, args);
+				context = args = null;
+			}
 
-      return result;
-    };
-  }
+			return result;
+		};
+	}
 
 };
 
