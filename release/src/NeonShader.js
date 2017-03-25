ea6f9d9c36339411d0be528088ca1a88cd137186
Jonathan Brandel <jono-vrse@Jonathans-MacBook-Pro-3.local>
2017-03-24 18:07:15 -0700

    updated floor color

--- release/src/NeonShader.js
+++ release/src/NeonShader.js
@@ -365,7 +365,7 @@ THREE.neonShader.swirlShader.blending = THREE.AdditiveBlending;
 
 THREE.neonShader.floorShader = THREE.neonShader.basicShader.clone();
 THREE.neonShader.floorShader.defines = { USE_FAKE_SHADOW: '' };
-THREE.neonShader.floorShader.uniforms.color.value.set( 0x72fa8c );
+THREE.neonShader.floorShader.uniforms.color.value.set( 0x08FA82 );
 
 THREE.neonShader.waveShader = THREE.neonShader.basicShader.clone();
 THREE.neonShader.waveShader.defines = { USE_WAVE: '' };
