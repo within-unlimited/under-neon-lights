ed6201f84c74da9409bd1e749e4151e97e4d87ca
Mr.doob <info@mrdoob.com>
2017-03-29 16:00:14 -0700

    WebAudio: Allow passing AudioContext.

--- release/src/WebAudio.js
+++ release/src/WebAudio.js
@@ -2,116 +2,110 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-var WebAudio = ( function () {
+function WebAudio( context ) {
 
-	var context;
+	if ( context === undefined ) {
 
-	return function () {
+		context = new ( window.AudioContext || window.webkitAudioContext )();
 
-		if ( context === undefined ) {
+	}
 
- 			context = new ( window.AudioContext || window.webkitAudioContext )();
+	var source, buffer;
 
-		}
+	var currentTime = 0;
+	var loop = false;
+	var playbackRate = 1;
 
-		var source, buffer;
+	var paused = true;
+	var startAt = 0;
 
-		var currentTime = 0;
-		var loop = false;
-		var playbackRate = 1;
+	function load( url ) {
 
-		var paused = true;
-		var startAt = 0;
-
-		function load( url ) {
-
-			var request = new XMLHttpRequest();
-			request.open( 'GET', url, true );
-			request.responseType = 'arraybuffer';
-			request.addEventListener( 'load', function ( event ) {
-				context.decodeAudioData( event.target.response, function ( data ) {
-					buffer = data;
-					if ( paused === false ) play();
-				} );
+		var request = new XMLHttpRequest();
+		request.open( 'GET', url, true );
+		request.responseType = 'arraybuffer';
+		request.addEventListener( 'load', function ( event ) {
+			context.decodeAudioData( event.target.response, function ( data ) {
+				buffer = data;
+				if ( paused === false ) play();
 			} );
-			request.send();
+		} );
+		request.send();
 
-		}
+	}
 
-		function getCurrentTime() {
+	function getCurrentTime() {
 
-			if ( buffer === undefined || paused === true ) return currentTime;
-			return currentTime + ( context.currentTime - startAt ) * playbackRate;
+		if ( buffer === undefined || paused === true ) return currentTime;
+		return currentTime + ( context.currentTime - startAt ) * playbackRate;
 
-		}
+	}
 
-		function play() {
+	function play() {
 
-			if ( buffer === undefined ) return;
+		if ( buffer === undefined ) return;
 
-			source = context.createBufferSource();
-			source.buffer = buffer;
-			source.loop = loop;
-			source.playbackRate.value = playbackRate;
-			source.start( 0, currentTime );
-			source.connect( context.destination );
+		source = context.createBufferSource();
+		source.buffer = buffer;
+		source.loop = loop;
+		source.playbackRate.value = playbackRate;
+		source.start( 0, currentTime );
+		source.connect( context.destination );
 
-			startAt = context.currentTime;
+		startAt = context.currentTime;
 
-		}
+	}
 
-		function stop() {
+	function stop() {
 
-			if ( buffer === undefined ) return;
+		if ( buffer === undefined ) return;
 
-			source.stop();
-			source.disconnect( context.destination );
+		source.stop();
+		source.disconnect( context.destination );
 
-			currentTime = getCurrentTime();
+		currentTime = getCurrentTime();
 
-		}
+	}
 
-		return {
-			play: function () {
-				if ( paused ) {
-					play(); paused = false;
-				}
-			},
-			pause: function () {
-				if ( paused === false ) {
-					stop(); paused = true;
-				}
-			},
-			get currentTime() {
-				return getCurrentTime();
-			},
-			set currentTime( value ) {
-				if ( paused === false ) stop();
-				currentTime = value;
-				if ( paused === false ) play();
-			},
-			get playbackRate() {
-				return playbackRate;
-			},
-			set playbackRate( value ) {
-				if ( paused === false ) stop();
-				playbackRate = value;
-				if ( paused === false ) play();
-			},
-			set src( url ) {
-				load( url );
-			},
-			get loop() {
-				return loop;
-			},
-			set loop( value ) {
-				loop = value;
-			},
-			get paused() {
-				return paused;
+	return {
+		play: function () {
+			if ( paused ) {
+				play(); paused = false;
 			}
+		},
+		pause: function () {
+			if ( paused === false ) {
+				stop(); paused = true;
+			}
+		},
+		get currentTime() {
+			return getCurrentTime();
+		},
+		set currentTime( value ) {
+			if ( paused === false ) stop();
+			currentTime = value;
+			if ( paused === false ) play();
+		},
+		get playbackRate() {
+			return playbackRate;
+		},
+		set playbackRate( value ) {
+			if ( paused === false ) stop();
+			playbackRate = value;
+			if ( paused === false ) play();
+		},
+		set src( url ) {
+			load( url );
+		},
+		get loop() {
+			return loop;
+		},
+		set loop( value ) {
+			loop = value;
+		},
+		get paused() {
+			return paused;
 		}
+	}
 
-	};
-
-} )()
+}
