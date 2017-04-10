8b5ca418e0d050deb47029b578fe43b32759fc26
Jonathan Brandel <jono@with.in>
2017-04-10 15:31:33 -0700

    Fixed particle fragment for Mac. Now to test on iOS

--- release/src/shaders/particle_frag.glsl
+++ release/src/shaders/particle_frag.glsl
@@ -1,10 +1,10 @@
+float PI = 3.141592653589793;
 uniform float progress;
 uniform vec3 col;
-
 varying vec3 vColor;
-
 void main() {
-	float gradient = max( 0.0, 1.0 - length( gl_PointCoord * 2.0 - vec2( 1.0 ) ) );
-	float alpha = min( 1.0, min( progress * 2.0  , ( 1.0 - progress ) * 3.0 ) );
-	gl_FragColor = vec4( col * vColor * vec3( gradient ), alpha );
+	float dist = length( gl_PointCoord * 2.0 - vec2( 1.0 ) );
+	float gradient = 1.0 - pow( dist, 2.0 );
+	float alpha = sin( progress * PI );
+	gl_FragColor = vec4( col, alpha * gradient );
 }
